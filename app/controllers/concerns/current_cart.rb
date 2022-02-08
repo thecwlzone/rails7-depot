e98e8e0101d2246f6cdf498f52772a223f688a0f
Christopher W Lehman <chris@thecwlzone.com>
2022-02-08 12:34:37 -0700

    Rubocop updates and subsequent code changes.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -7,13 +7,12 @@
 # Visit https://pragprog.com/titles/rails7 for more book information.
 #---
 module CurrentCart
-
   private
 
-    def set_cart 
-      @cart = Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+  end
 end

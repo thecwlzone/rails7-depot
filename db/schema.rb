432d2cd6fcde3c11a3278b4d2f7553b011d72c9f
Christopher W Lehman <chris@thecwlzone.com>
2022-03-25 13:30:18 -0600

    Iteration I1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_201858) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_25_191220) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +46,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_201858) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
